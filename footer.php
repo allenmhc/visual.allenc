1280e4fb70520eb096549167bdc48246b58564d5
Allen Cheung <me@allenc.com>
2014-02-09 00:08:33 -0800

    Footer styling.
    Adding social icons, copyright.

--- footer.php
+++ footer.php
@@ -14,9 +14,51 @@
 </div><!-- #page .hfeed .site -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-  <div class="section clearfix">
-    <?php do_action( 'visual_footer_text' ); ?>
-  </div><!-- .site-info -->
+
+  <div class="section" id="social">
+    <ul id="social-icons" class="clearfix">
+      <li class="social-icon">
+        <a href="http://twitter.com/#!/allenmhc" target="_blank" title="Twitter" class="twitter" rel="me">
+          <span>Twitter</span>
+        </a>
+      </li>
+      <li class="social-icon">
+        <a rel="author" target="_blank" title="Google+" class="googleplus"
+           rel="me" href="https://profiles.google.com/allenc">
+          <span>Google+</span>
+        </a>
+      </li>
+      <li class="social-icon">
+        <a href="http://www.quora.com/Allen-Cheung" target="_blank" title="Quora" class="quora" rel="me">
+          <span>Quora</span>
+        </a>
+      </li>
+      <li class="social-icon">
+        <a href="http://www.linkedin.com/in/allencheung" target="_blank" title="LinkedIn" class="linkedin" rel="me">
+          <span>LinkedIn</span>
+        </a>
+      </li>
+      <li class="social-icon">
+        <a href="https://github.com/allenmhc" target="_blank" title="Github" class="github" rel="me">
+          <span>Github</span>
+        </a>
+      </li>
+      <li class="social-icon">
+        <a id="rss" title="rss" class="rss" href="<?php bloginfo('rss2_url'); ?>">
+          <span>RSS</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="section clearfix" id="copyright">
+    <span>copyright &copy;&nbsp;<?php
+      $start_year = 2011;
+      $curr_year = date('Y');
+      echo $start_year . ($start_year == $curr_year ? '' : '-' . $curr_year);
+    ?> allenc.com</span>
+  </div>
+
 </footer><!-- #colophon .site-footer -->
 
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -158,7 +158,6 @@ function visual_fonts() {
     $font_families[] = 'Lato:100,300,400,900';
     $font_families[] = 'Titillium+Web:300,400,700';
     $font_families[] = 'Open+Sans:400,700,400italic,700italic';
-    $font_families[] = 'PT+Sans:400,700,400italic,700italic';
     $protocol = is_ssl() ? 'https' : 'http';
     $query_args = array(
       'family' => implode( '|', $font_families ),
