0c182fcce6c27db590c4afad81b4c7a15f571588
Allen Cheung <me@allenc.com>
2014-02-14 01:17:31 -0800

    Fix home page to take into account the paged var.
    Otherwise back/forward doesn't work.

--- home.php
+++ home.php
@@ -10,28 +10,35 @@ get_header(); ?>
 
   <div id="primary" class="content-area">
     <div id="content" class="site-content" role="main">
-        <!-- featured post -->
         <?php
-        rewind_posts();
-        $query_args = array(
-          "meta_key" => "article",
-          "meta_value" => 1,
-          "posts_per_page" => 1
-        );
-        query_posts($query_args);
-        the_post();
-        $featured_post_id = get_the_ID();
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $post_count = 0;
         ?>
-        <?php get_template_part( 'content-featured', get_post_format() ); ?>
+        <!-- featured post -->
+        <?php if ($paged == 1): ?>
+          <?php
+          rewind_posts();
+          $query_args = array(
+            "meta_key" => "article",
+            "meta_value" => 1,
+            "posts_per_page" => 1
+          );
+          query_posts($query_args);
+          the_post();
+          $featured_post_id = get_the_ID();
+          ?>
+          <?php get_template_part( 'content-featured', get_post_format() ); ?>
+          <?php $post_count++; ?>
+        <?php endif; ?>
 
         <!-- other posts -->
         <?php
         rewind_posts();
         $query_args = array(
-          "posts_per_page" => 12
+          "posts_per_page" => 12,
+          "paged" => $paged
         );
         query_posts($query_args);
-        $post_count = 1;
         while (have_posts()): the_post();
         ?>
           <?php if ($post_count < 12 && get_the_ID() != $featured_post_id): ?>
