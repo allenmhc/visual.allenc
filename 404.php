cbc258e4d924b32d45955ccac43568e82ebdcc01
Allen Cheung <me@allenc.com>
2014-03-09 01:25:03 -0800

    404 page and styling.

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying 404 pages (Not Found).
  *
- * @package Visual
+ * @package Visual.allenc
  * @since Visual 0.1
  */
 
@@ -10,24 +10,21 @@ get_header(); ?>
 
   <div id="primary" class="content-area">
     <div id="content" class="site-content" role="main">
+      <article class="fourofour">
+        <div class="fourofour-bg"></div>
+        <div class="fourofour-content clearfix">
+          <h2>Whoops.</h2>
+          <h3>Something doesn't seem quite right here.</h3>
 
-      <article id="post-0" class="post error404 not-found hentry">
-        <header class="entry-header">
-          <h1 class="entry-title"><?php _e( 'Oops! That page can&apos;t be found.', 'visual' ); ?></h1>
-        </header><!-- .entry-header -->
-
-        <div class="entry-content">
-          <p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'visual' ); ?></p>
-
-          <?php get_search_form(); ?>
-
-          <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-        </div><!-- .entry-content -->
-      </article><!-- #post-0 .post .error404 .not-found -->
+          <p class="left">We can sit here and look at each other suspiciously for a while.</p>
+          <p class="right">
+            Or instead, how about you <a href="<?php home_url(); ?>">return home</a> and
+            we call it even?
+          </p>
+        </div>
+      </article>
 
     </div><!-- #content .site-content -->
   </div><!-- #primary .content-area -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
